b4a22d3b125ae11672c83e5ecd8c2abc8061a8f5
ANJINAYULU <anji28257@gmail.com>
2026-02-26 11:18:20 +0530

    seventh  time run the code

--- .history/main_20260226111530.tf
+++ .history/main_20260226111530.tf
@@ -1,11 +1,5 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+
 
 
 
--- .history/main_20260226111644.tf
+++ .history/main_20260226111644.tf
@@ -1,11 +1,4 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 
 
 
